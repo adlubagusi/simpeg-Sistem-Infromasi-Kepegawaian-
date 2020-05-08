271c0addf28a40bc3ec065b03b1bfc2d85da4846
Adlu Bagus Irawan <adlubagusi@gmail.com>
2020-05-09 05:33:00 +0800

    update database

--- pegawai.sql
+++ pegawai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Bulan Mei 2020 pada 06.09
+-- Waktu pembuatan: 08 Bulan Mei 2020 pada 23.31
 -- Versi server: 10.1.31-MariaDB
 -- Versi PHP: 7.2.3
 
@@ -21,6 +21,85 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_pegawai`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `absen`
+--
+
+CREATE TABLE `absen` (
+  `id_absen` int(11) NOT NULL,
+  `id_pegawai` int(100) NOT NULL,
+  `hadir` int(100) NOT NULL,
+  `izin` int(100) NOT NULL,
+  `tidak_hadir` int(100) NOT NULL,
+  `bulan` int(100) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `absen`
+--
+
+INSERT INTO `absen` (`id_absen`, `id_pegawai`, `hadir`, `izin`, `tidak_hadir`, `bulan`, `tanggal`) VALUES
+(10, 8, 30, 0, 0, 1, '2018-04-02'),
+(12, 9, 10, 10, 10, 5, '2020-05-01');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `level` varchar(100) NOT NULL,
+  `log` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama`, `level`, `log`) VALUES
+(2, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin', '2018-03-30 02:51:21'),
+(3, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'User', 'user', '2018-04-02 18:27:39');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jabatan`
+--
+
+CREATE TABLE `jabatan` (
+  `id_jabatan` int(11) NOT NULL,
+  `nama_jabatan` varchar(100) NOT NULL,
+  `golongan` varchar(100) NOT NULL,
+  `tunjangan` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `jabatan`
+--
+
+INSERT INTO `jabatan` (`id_jabatan`, `nama_jabatan`, `golongan`, `tunjangan`) VALUES
+(5, 'Frontend Developer', 'Junior', 2500000),
+(6, 'Backend Developer', 'Junior', 2800000),
+(7, 'Database Admin', 'Senior', 5700000),
+(8, 'Frontend Developer', 'Middle', 5200000),
+(9, 'Backend Developer', 'Middle', 5500000),
+(10, 'Frontend Developer', 'Senior', 8550000),
+(11, 'Backend Developer', 'Senior', 9325700),
+(12, 'Staff IT', 'Senior', 3500000),
+(13, 'General Manager', '-', 12750000),
+(14, 'Network Engineer', 'Senior', 8500000),
+(15, 'Admin Server', 'Senior', 8750000),
+(16, 'Graphic Designer', 'Senior', 7500000),
+(17, 'Graphic Designer', 'Junior', 2200000);
 
 -- --------------------------------------------------------
 
@@ -50,9 +129,51 @@ CREATE TABLE `pegawai` (
 INSERT INTO `pegawai` (`id_pegawai`, `id_jabatan`, `nip`, `nama`, `jk`, `foto`, `agama`, `pendidikan`, `status_kep`, `alamat`, `username`, `password`) VALUES
 (9, 13, '200108312018110801002', 'Adlu Bagus Irawan', '0', 'foto_1588345007.jpg', 'islam', 'SMK RPL', 'Tetap', 'Jl Gatot Subroto VI F, Denpasar, Bali', 'adlubagusi', '827ccb0eea8a706c4c34a16891f84e7b');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tpp`
+--
+
+CREATE TABLE `tpp` (
+  `id_tpp` int(11) NOT NULL,
+  `id_pegawai` int(100) NOT NULL,
+  `jumlah_tpp` varchar(100) NOT NULL,
+  `jumlah_potongan` varchar(100) NOT NULL,
+  `bulan_t` int(100) NOT NULL,
+  `tahun` int(100) NOT NULL,
+  `tgl` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tpp`
+--
+
+INSERT INTO `tpp` (`id_tpp`, `id_pegawai`, `jumlah_tpp`, `jumlah_potongan`, `bulan_t`, `tahun`, `tgl`) VALUES
+(7, 8, '300000', '0%', 1, 2018, '2018-04-02'),
+(9, 9, '12750000', '0%', 5, 2020, '2020-05-01');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `absen`
+--
+ALTER TABLE `absen`
+  ADD PRIMARY KEY (`id_absen`);
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indeks untuk tabel `jabatan`
+--
+ALTER TABLE `jabatan`
+  ADD PRIMARY KEY (`id_jabatan`);
 
 --
 -- Indeks untuk tabel `pegawai`
@@ -61,14 +182,44 @@ ALTER TABLE `pegawai`
   ADD PRIMARY KEY (`id_pegawai`);
 
 --
+-- Indeks untuk tabel `tpp`
+--
+ALTER TABLE `tpp`
+  ADD PRIMARY KEY (`id_tpp`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `absen`
+--
+ALTER TABLE `absen`
+  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `jabatan`
+--
+ALTER TABLE `jabatan`
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT untuk tabel `pegawai`
 --
 ALTER TABLE `pegawai`
   MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `tpp`
+--
+ALTER TABLE `tpp`
+  MODIFY `id_tpp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
